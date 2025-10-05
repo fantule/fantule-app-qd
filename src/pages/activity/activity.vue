f15a4aedbfabf946fdc9ab82d4f0c8935be79c38
李凯明 <IAM_970924196@csair.com>
2025-10-05 10:50:23 +0800

    update

--- src/pages/activity/activity.vue
+++ src/pages/activity/activity.vue
@@ -1,47 +1,43 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">123</text>
-    </view>
+  <view class="activity">
+	<uni-nav-bar
+	    :fixed="true"
+	    status-bar
+		backgroundColor="transparent"
+	    left-icon="left"
+	    left-text=""
+	    title="活动中心"
+		:border="false"
+		:shadow="false"
+	/>
+	<view class="card-content">
+		<image class="logo-img" src="/static/home/list1.png" />
+		<view class="text1">CYF次元嘉年华动漫展</view>
+		<view class="bottom-wrap">
+			<view class="text-time">活动时间:2024 8.22 10:00-19:00</view>
+			<view class="text-see">
+				查看更多<uni-icons type="right" class="right-icon" /><uni-icons type="right" class="right-icon" />
+			</view>
+		</view>
+	</view>
+	<view class="card-content">
+		<image class="logo-img" src="/static/home/list1.png" />
+		<view class="text1">CYF次元嘉年华动漫展</view>
+		<view class="bottom-wrap">
+			<view class="text-time">活动时间:2024 8.22 10:00-19:00</view>
+			<view class="text-see">
+				查看更多<uni-icons type="right" class="right-icon" /><uni-icons type="right" class="right-icon" />
+			</view>
+		</view>
+	</view>
   </view>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue'
-// import { useRouter } from 'vue-router'
-// const title = ref('Hello')
-// const router = useRouter()
-//
-// const navigateToDetail = () => {
-//   router.push('/home')
-// };
+
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+<style scoped lang="less">
+@import url('./activity.less');
 </style>
+
--- src/pages/community/community.vue
+++ src/pages/community/community.vue
@@ -3,6 +3,34 @@
     <hedTop
         title="社群"
     />
+	<view class="card">
+		<view class="background-wrap">
+			<view class="card-text-h1">我的足迹</view>
+			<view class="card-text-h2">社群浏览记录</view>
+		</view>
+		<view class="background-wrap">
+			<view class="card-text-h1">我的收藏</view>
+			<view class="card-text-h2">收藏的社群</view>
+		</view>
+	</view>
+	<view class="content-wrap">
+		 <image class="logo-img" src="/static/home/starlights@2x.png" />
+		 <view class="mid-wrap">
+			 <view class="text1">bjd娃娃交流</view>
+			 <view class="text2">当前该社群用户:152</view>
+			 <view class="text2">这里是bjd娃娃分类的内容…</view>
+		 </view>
+		 <button class="mini-btn add-btn" type="default" size="mini">加入</button>
+	</view>
+	<view class="content-wrap">
+		 <image class="logo-img" src="/static/home/starlights@2x.png" />
+		 <view class="mid-wrap">
+			 <view class="text1">bjd娃娃交流</view>
+			 <view class="text2">当前该社群用户:152</view>
+			 <view class="text2">这里是bjd娃娃分类的内容…</view>
+		 </view>
+		 <button class="mini-btn add-btn" type="default" size="mini">加入</button>
+	</view>
   </view>
 </template>
 
